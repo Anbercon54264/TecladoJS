b3f9170997ed07372a93acaa728a82a01df74983
Andres Bernal <anbercon@hotmail.com>
2021-03-17 20:53:53 -0500

    Segundo Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let divTexto = document.getElementById("divTexto")
+let divTexto = document.getElementById("divTexto");
 //Numeros
 let btnNum1 = document.getElementById("btnNum1");
 let btnNum2 = document.getElementById("btnNum2");
@@ -12,42 +12,398 @@ let btnNum9 = document.getElementById("btnNum9");
 let btnNum0 = document.getElementById("btnNum0");
 // letras
 
-let btnQ = document.getElementById("btnQ")
-let btnW = document.getElementById("btnW")
-let btnE = document.getElementById("btnE")
-let btnR = document.getElementById("btnR")
-let btnT = document.getElementById("btnT")
-let btnY = document.getElementById("btnY")
-let btnU = document.getElementById("btnU")
-let btnI = document.getElementById("btnI")
-let btnO = document.getElementById("btnO")
-let btnP = document.getElementById("btnP")
-let btnA = document.getElementById("btnA")
-let btnS = document.getElementById("btnS")
-let btnD = document.getElementById("btnD")
-let btnF = document.getElementById("btnF")
-let btnG = document.getElementById("btnG")
-let btnH = document.getElementById("btnH")
-let btnJ = document.getElementById("btnJ")
-let btnK = document.getElementById("btnK")
-let btnL = document.getElementById("btnL")
-let btnÑ = document.getElementById("btnÑ")
-let btnZ = document.getElementById("btnZ")
-let btnX = document.getElementById("btnX")
-let btnC = document.getElementById("btnC")
-let btnV = document.getElementById("btnV")
-let btnB = document.getElementById("btnB")
-let btnN = document.getElementById("btnN")
-let btnM = document.getElementById("btnM")
+let btnQ = document.getElementById("btnQ");
+let btnW = document.getElementById("btnW");
+let btnE = document.getElementById("btnE");
+let btnR = document.getElementById("btnR");
+let btnT = document.getElementById("btnT");
+let btnY = document.getElementById("btnY");
+let btnU = document.getElementById("btnU");
+let btnI = document.getElementById("btnI");
+let btnO = document.getElementById("btnO");
+let btnP = document.getElementById("btnP");
+let btnA = document.getElementById("btnA");
+let btnS = document.getElementById("btnS");
+let btnD = document.getElementById("btnD");
+let btnF = document.getElementById("btnF");
+let btnG = document.getElementById("btnG");
+let btnH = document.getElementById("btnH");
+let btnJ = document.getElementById("btnJ");
+let btnK = document.getElementById("btnK");
+let btnL = document.getElementById("btnL");
+let btnÑ = document.getElementById("btnÑ");
+let btnZ = document.getElementById("btnZ");
+let btnX = document.getElementById("btnX");
+let btnC = document.getElementById("btnC");
+let btnV = document.getElementById("btnV");
+let btnB = document.getElementById("btnB");
+let btnN = document.getElementById("btnN");
+let btnM = document.getElementById("btnM");
 
 //Otras
 
-let btnEnter = document.getElementById("btnEnter")
-let btnComa = document.getElementById("btnComa")
-let btnPunto= document.getElementById("btnPunto")
-let btnGuion = document.getElementById("btnGuion")
-let btnEspacio = document.getElementById("btnEspacio")
-let btnBorrar = document.getElementById("btnBorrar")
+let btnEnter = document.getElementById("btnEnter");
+let btnComa = document.getElementById("btnComa");
+let btnPunto = document.getElementById("btnPunto");
+let btnGuion = document.getElementById("btnGuion");
+let btnEspacio = document.getElementById("btnEspacio");
+let btnBorrar = document.getElementById("btnBorrar");
+
+//
+let tmpLetras = "";
 
 //Funciones
 
+const FbtnNum1 = () => {
+  divTexto.innerHTML = tmpLetras + "1";
+  tmpLetras = tmpLetras + "1";
+};
+
+const FbtnNum2 = () => {
+  divTexto.innerHTML = tmpLetras + "2";
+  tmpLetras = tmpLetras + "2";
+};
+
+const FbtnNum3 = () => {
+  divTexto.innerHTML = tmpLetras + "3";
+  tmpLetras = tmpLetras + "3";
+};
+
+const FbtnNum4 = () => {
+  divTexto.innerHTML = tmpLetras + "4";
+  tmpLetras = tmpLetras + "4";
+};
+
+const FbtnNum5 = () => {
+  divTexto.innerHTML = tmpLetras + "5";
+  tmpLetras = tmpLetras + "5";
+};
+
+const FbtnNum6 = () => {
+  divTexto.innerHTML = tmpLetras + "6";
+  tmpLetras = tmpLetras + "6";
+};
+
+const FbtnNum7 = () => {
+  divTexto.innerHTML = tmpLetras + "7";
+  tmpLetras = tmpLetras + "7";
+};
+
+const FbtnNum8 = () => {
+  divTexto.innerHTML = tmpLetras + "8";
+  tmpLetras = tmpLetras + "8";
+};
+
+const FbtnNum9 = () => {
+  divTexto.innerHTML = tmpLetras + "9";
+  tmpLetras = tmpLetras + "9";
+};
+
+const FbtnNum0 = () => {
+  divTexto.innerHTML = tmpLetras + "0";
+  tmpLetras = tmpLetras + "0";
+};
+
+const fBtnA = () => {
+  divTexto.innerHTML = tmpLetras + "A";
+  tmpLetras = tmpLetras + "A";
+};
+
+const fBtnB = () => {
+  divTexto.innerHTML = tmpLetras + "B";
+  tmpLetras = tmpLetras + "B";
+};
+
+const fBtnC = () => {
+  divTexto.innerHTML = tmpLetras + "C";
+  tmpLetras = tmpLetras + "C";
+};
+
+const fBtnD = () => {
+  divTexto.innerHTML = tmpLetras + "D";
+  tmpLetras = tmpLetras + "D";
+};
+
+const fBtnE = () => {
+  divTexto.innerHTML = tmpLetras + "E";
+  tmpLetras = tmpLetras + "E";
+};
+
+const fBtnF = () => {
+  divTexto.innerHTML = tmpLetras + "F";
+  tmpLetras = tmpLetras + "F";
+};
+
+const fBtnG = () => {
+  divTexto.innerHTML = tmpLetras + "G";
+  tmpLetras = tmpLetras + "G";
+};
+
+const fBtnH = () => {
+  divTexto.innerHTML = tmpLetras + "H";
+  tmpLetras = tmpLetras + "H";
+};
+
+const fBtnI = () => {
+  divTexto.innerHTML = tmpLetras + "I";
+  tmpLetras = tmpLetras + "I";
+};
+
+const fBtnJ = () => {
+  divTexto.innerHTML = tmpLetras + "J";
+  tmpLetras = tmpLetras + "J";
+};
+
+const fBtnK = () => {
+  divTexto.innerHTML = tmpLetras + "K";
+  tmpLetras = tmpLetras + "K";
+};
+
+const fBtnL = () => {
+  divTexto.innerHTML = tmpLetras + "L";
+  tmpLetras = tmpLetras + "L";
+};
+
+const fBtnM = () => {
+  divTexto.innerHTML = tmpLetras + "M";
+  tmpLetras = tmpLetras + "M";
+};
+
+const fBtnN = () => {
+  divTexto.innerHTML = tmpLetras + "N";
+  tmpLetras = tmpLetras + "N";
+};
+
+const fBtnÑ = () => {
+  divTexto.innerHTML = tmpLetras + "Ñ";
+  tmpLetras = tmpLetras + "Ñ";
+};
+
+const fBtnO = () => {
+  divTexto.innerHTML = tmpLetras + "O";
+  tmpLetras = tmpLetras + "O";
+};
+
+const fBtnP = () => {
+  divTexto.innerHTML = tmpLetras + "P";
+  tmpLetras = tmpLetras + "P";
+};
+
+const fBtnQ = () => {
+  divTexto.innerHTML = tmpLetras + "Q";
+  tmpLetras = tmpLetras + "Q";
+};
+
+const fBtnR = () => {
+  divTexto.innerHTML = tmpLetras + "R";
+  tmpLetras = tmpLetras + "R";
+};
+
+const fBtnS = () => {
+  divTexto.innerHTML = tmpLetras + "S";
+  tmpLetras = tmpLetras + "S";
+};
+
+const fBtnT = () => {
+  divTexto.innerHTML = tmpLetras + "T";
+  tmpLetras = tmpLetras + "B";
+};
+
+const fBtnU = () => {
+  divTexto.innerHTML = tmpLetras + "U";
+  tmpLetras = tmpLetras + "U";
+};
+
+const fBtnV = () => {
+  divTexto.innerHTML = tmpLetras + "V";
+  tmpLetras = tmpLetras + "V";
+};
+
+const fBtnW = () => {
+  divTexto.innerHTML = tmpLetras + "W";
+  tmpLetras = tmpLetras + "W";
+};
+
+const fBtnX = () => {
+  divTexto.innerHTML = tmpLetras + "X";
+  tmpLetras = tmpLetras + "X";
+};
+const fBtnY = () => {
+  divTexto.innerHTML = tmpLetras + "Y";
+  tmpLetras = tmpLetras + "Y";
+};
+
+const fBtnZ = () => {
+  divTexto.innerHTML = tmpLetras + "Z";
+  tmpLetras = tmpLetras + "Z";
+};
+
+const fBtnBorrar = () => {
+  tmpLetras = "";
+  divTexto.innerHTML = "";
+};
+
+const fBtnPunto = () => {
+  divTexto.innerHTML = tmpLetras + ".";
+  tmpLetras = tmpLetras + ".";
+};
+
+const fBtnComa = () => {
+  divTexto.innerHTML = tmpLetras + ",";
+  tmpLetras = tmpLetras + ",";
+};
+
+const fBtnEspacio = () => {
+  divTexto.innerHTML = tmpLetras + " ";
+  tmpLetras = tmpLetras + " ";
+};
+
+const fBtnGuion = () => {
+  divTexto.innerHTML = tmpLetras + "-";
+  tmpLetras = tmpLetras + "-";
+};
+
+const fBtnEnter = () => {
+  divTexto.innerHTML = tmpLetras + "\n";
+  tmpLetras = tmpLetras + "\n";
+};
+
+btnNum1.onclick = function () {
+  FbtnNum1();
+};
+
+btnNum2.onclick = function () {
+  FbtnNum2();
+};
+
+btnNum3.onclick = function () {
+  FbtnNum3();
+};
+
+btnNum4.onclick = function () {
+  FbtnNum4();
+};
+
+btnNum5.onclick = function () {
+  FbtnNum5();
+};
+
+btnNum6.onclick = function () {
+  FbtnNum6();
+};
+
+btnNum7.onclick = function () {
+  FbtnNum7();
+};
+
+btnNum8.onclick = function () {
+  FbtnNum8();
+};
+
+btnNum9.onclick = function () {
+  FbtnNum9();
+};
+
+btnNum0.onclick = function () {
+  FbtnNum0();
+};
+
+btnQ.onclick = function () {
+  fBtnQ();
+};
+btnW.onclick = function () {
+  fBtnW();
+};
+btnE.onclick = function () {
+  fBtnE();
+};
+btnR.onclick = function () {
+  fBtnR();
+};
+btnT.onclick = function () {
+  fBtnT();
+};
+btnY.onclick = function () {
+  fBtnY();
+};
+btnU.onclick = function () {
+  fBtnU();
+};
+btnI.onclick = function () {
+  fBtnI();
+};
+btnO.onclick = function () {
+  fBtnO();
+};
+btnP.onclick = function () {
+  fBtnP();
+};
+btnA.onclick = function () {
+  fBtnA();
+};
+btnS.onclick = function () {
+  fBtnS();
+};
+btnD.onclick = function () {
+  fBtnD();
+};
+btnF.onclick = function () {
+  fBtnF();
+};
+btnG.onclick = function () {
+  fBtnG();
+};
+btnH.onclick = function () {
+  fBtnH();
+};
+btnJ.onclick = function () {
+  fBtnJ();
+};
+btnK.onclick = function () {
+  fBtnK();
+};
+btnL.onclick = function () {
+  fBtnL();
+};
+btnÑ.onclick = function () {
+  fBtnÑ();
+};
+btnZ.onclick = function () {
+  fBtnZ();
+};
+btnX.onclick = function () {
+  fBtnX();
+};
+btnC.onclick = function () {
+  fBtnC();
+};
+btnV.onclick = function () {
+  fBtnV();
+};
+btnB.onclick = function () {
+  fBtnB();
+};
+btnN.onclick = function () {
+  fBtnN();
+};
+btnM.onclick = function () {
+  fBtnM();
+};
+btnComa.onclick = function () {
+  fBtnComa();
+};
+btnPunto.onclick = function () {
+  fBtnPunto();
+};
+btnGuion.onclick = function () {
+  fBtnGuion();
+};
+btnBorrar.onclick = function () {
+  fBtnBorrar();
+};
+btnEspacio.onclick = function () {
+  fBtnEspacio();
+};
+btnEnter.onclick = function () {
+  fBtnEnter();
+};
