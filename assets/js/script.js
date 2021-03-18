93e6d82662f569186abbe6903545a16353accc7e
Andres Bernal <anbercon@hotmail.com>
2021-03-17 19:40:02 -0500

    Primer Commit

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+let divTexto = document.getElementById("divTexto")
+//Numeros
+let btnNum1 = document.getElementById("btnNum1");
+let btnNum2 = document.getElementById("btnNum2");
+let btnNum3 = document.getElementById("btnNum3");
+let btnNum4 = document.getElementById("btnNum4");
+let btnNum5 = document.getElementById("btnNum5");
+let btnNum6 = document.getElementById("btnNum6");
+let btnNum7 = document.getElementById("btnNum7");
+let btnNum8 = document.getElementById("btnNum8");
+let btnNum9 = document.getElementById("btnNum9");
+let btnNum0 = document.getElementById("btnNum0");
+// letras
+
+let btnQ = document.getElementById("btnQ")
+let btnW = document.getElementById("btnW")
+let btnE = document.getElementById("btnE")
+let btnR = document.getElementById("btnR")
+let btnT = document.getElementById("btnT")
+let btnY = document.getElementById("btnY")
+let btnU = document.getElementById("btnU")
+let btnI = document.getElementById("btnI")
+let btnO = document.getElementById("btnO")
+let btnP = document.getElementById("btnP")
+let btnA = document.getElementById("btnA")
+let btnS = document.getElementById("btnS")
+let btnD = document.getElementById("btnD")
+let btnF = document.getElementById("btnF")
+let btnG = document.getElementById("btnG")
+let btnH = document.getElementById("btnH")
+let btnJ = document.getElementById("btnJ")
+let btnK = document.getElementById("btnK")
+let btnL = document.getElementById("btnL")
+let btnÑ = document.getElementById("btnÑ")
+let btnZ = document.getElementById("btnZ")
+let btnX = document.getElementById("btnX")
+let btnC = document.getElementById("btnC")
+let btnV = document.getElementById("btnV")
+let btnB = document.getElementById("btnB")
+let btnN = document.getElementById("btnN")
+let btnM = document.getElementById("btnM")
+
+//Otras
+
+let btnEnter = document.getElementById("btnEnter")
+let btnComa = document.getElementById("btnComa")
+let btnPunto= document.getElementById("btnPunto")
+let btnGuion = document.getElementById("btnGuion")
+let btnEspacio = document.getElementById("btnEspacio")
+let btnBorrar = document.getElementById("btnBorrar")
+
+//Funciones
+
